3c0fd6f53825b94b5eac50b42c4a6294b3f06a7f
danielmoralesp <daniel_moralesp@hotmail.com>
2017-03-16 09:52:07 -0500

    remove data of some migrations

--- db/migrate/20170315234338_remove_hours_from_gigs.rb
+++ db/migrate/20170315234338_remove_hours_from_gigs.rb
@@ -1,10 +1,5 @@
 class RemoveHoursFromGigs < ActiveRecord::Migration
   def change
-    remove_column :gigs, :hours, :integer
-    remove_column :gigs, :days, :integer
-    remove_column :gigs, :week, :integer
-    remove_column :gigs, :month, :integer
-    remove_column :gigs, :experience, :text
-    remove_column :gigs, :responsabilities, :text
+
   end
 end
--- db/migrate/20170316000839_remove_hours_from_time_offs.rb
+++ db/migrate/20170316000839_remove_hours_from_time_offs.rb
@@ -1,10 +1,5 @@
 class RemoveHoursFromTimeOffs < ActiveRecord::Migration
   def change
-    remove_column :time_offs, :hours, :integer
-    remove_column :time_offs, :days, :integer
-    remove_column :time_offs, :week, :integer
-    remove_column :time_offs, :month, :integer
-    remove_column :time_offs, :experience, :text
-    remove_column :time_offs, :responsabilities, :text
+
   end
 end
